745ae4d63356800820b0de048db2885dce996184
jagannathbag99 <jagannathbag99@gmail.com>
2018-05-05 22:09:33 +0530

    init

--- app/src/main/java/com/example/www/example/MainActivity.java
+++ app/src/main/java/com/example/www/example/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView t2;
-    Button b1,b2;
+    Button b1,b2,b3,b4;
 
 
     @Override
@@ -19,22 +19,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         t2 = (TextView)findViewById(R.id.t2);
         b1 = (Button)findViewById(R.id.b1);
-        b2 = (Button)findViewById(R.id.b2);
-
-
-        b1.setOnClickListener(this);
+        b2=(Button)findViewById(R.id.b2);
+        b3 =(Button)findViewById(R.id.b3);
+        b4 = (Button)findViewById(R.id.b4);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t2.setText("You clicked Button1");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t2.setText("You clicked Button3");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t2.setText("You clicked Button4");
+            }
+        });
 
 
 
     }
+    public void cb2(View v){
+        t2.setText("You clicked button2");
+    }
+
+
+
 
     @Override
     public void onClick(View v) {
-        t2.setText("You clicked button 1");
-
 
     }
-
-
-
 }
